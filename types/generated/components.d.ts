c83ccf450dff96aa0bcb8feb725af8975d3beb1b
Jairo Gatjens <jgatjens@gmail.com>
2026-09-21 09:46:21 +0200

    add categories selector component and integrate with work detail

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,24 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface SharedCategoriesSelector extends Schema.Component {
+  collectionName: 'components_shared_categories_selector';
+  info: {
+    displayName: 'Categories';
+    icon: 'checkSquare';
+    description: 'Select project categories using checkboxes';
+  };
+  attributes: {
+    web: Attribute.Boolean & Attribute.DefaultTo<false>;
+    mobile: Attribute.Boolean & Attribute.DefaultTo<false>;
+    backend: Attribute.Boolean & Attribute.DefaultTo<false>;
+    headless_cms: Attribute.Boolean & Attribute.DefaultTo<false>;
+    animations: Attribute.Boolean & Attribute.DefaultTo<false>;
+    cms: Attribute.Boolean & Attribute.DefaultTo<false>;
+    design_systems: Attribute.Boolean & Attribute.DefaultTo<false>;
+    ai: Attribute.Boolean & Attribute.DefaultTo<false>;
+  };
+}
+
 export interface SharedCtalink extends Schema.Component {
   collectionName: 'components_shared_ctalinks';
   info: {
@@ -94,12 +113,14 @@ export interface SharedWorkDetail extends Schema.Component {
           localized: true;
         };
       }>;
+    categories: Attribute.Component<'shared.categories-selector'>;
   };
 }
 
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
+      'shared.categories-selector': SharedCategoriesSelector;
       'shared.ctalink': SharedCtalink;
       'shared.experience': SharedExperience;
       'shared.opengraph': SharedOpengraph;
